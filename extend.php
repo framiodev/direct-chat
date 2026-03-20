2f14fdc23f9f03613e371da1f5e58bda035b138a
Framio Dev <dev@framiomedia.com>
2026-03-20 22:39:46 +0300

    Admin beyaz ekran hatasını çözmek için admin assetleri kaldırıldı.

--- extend.php
+++ extend.php
@@ -17,11 +17,6 @@ return [
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less'),
         
-    // Admin Paneli (Yönetim) javascript ve css dosyalarını dahil ediyoruz
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/less/admin.less'),
-        
     // Dil dosyalarının nerede olduğunu belirtiyoruz
     new Extend\Locales(__DIR__.'/locale'),
 
